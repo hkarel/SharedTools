70f4c6acdd9a2082c646f615b8e9befee54e398e
Pavel Karelin <hkarel@yaned.ru>
2015-03-01 21:25:03 +0300

    break_point : Функция break_point() исключена при компиляции под mingw

--- break_point.h
+++ break_point.h
@@ -12,31 +12,33 @@ void sigtrap_handler(int signo)
 
 int debug_init()
 {
-	return signal(SIGTRAP, sigtrap_handler) == SIG_ERR ? -1 : 0;
+    return signal(SIGTRAP, sigtrap_handler) == SIG_ERR ? -1 : 0;
 }
 
 #define DebugBreak() {asm volatile("int $3");}
 
 int main ()
 {
-	int a, b, c, max;
-	debug_init();
-	scanf("%d %d %d", &a, &b, &c);
-	if (a > b)
-		max = a;
-	else
-		max = b;
-	DebugBreak();
-	if (c > max)
-		max = c;
-	printf("%d\n", max);
-	return 0;
+    int a, b, c, max;
+    debug_init();
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b)
+        max = a;
+    else
+        max = b;
+    DebugBreak();
+    if (c > max)
+        max = c;
+    printf("%d\n", max);
+    return 0;
 }
 */
 
 #ifndef NDEBUG
-  #ifdef _MSC_VER
+  #if defined(_MSC_VER)
     #define break_point  _CrtDbgBreak();
+  #elif defined(_MINGW)
+    #define break_point
   #else
     #include <stdio.h>
     #include <signal.h>
@@ -53,7 +55,7 @@ int main ()
     } static init_break_point;
     //#define break_point  {asm volatile("int $3");}
     #define break_point  {asm volatile("int $3" ::: "cc", "memory");}
-  #endif // _MSC_VER
+  #endif
 #else
     #define break_point
 #endif
