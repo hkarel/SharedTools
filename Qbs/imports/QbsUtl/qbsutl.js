190895b0383d542989420ae0a7dd5783b117bf9b
Pavel Karelin <hkarel@yandex.ru>
2016-02-06 20:27:57 +0300

    qbsutl.js : Добавлена функция getVersions(), используется для определения версии проекта.

--- Qbs/imports/QbsUtl/qbsutl.js
+++ Qbs/imports/QbsUtl/qbsutl.js
@@ -21,3 +21,37 @@ String.prototype.format = function () {
         return args[n];
     });
 };
+
+// Функция возвращает информацию о версии проекта в виде массива из пяти элементов.
+// Массив имеет следующие элементы:
+// 0 - версия проекта в формате major.minor.patch
+// 1 - major версия
+// 2 - minor версия
+// 3 - patch (версия исправлений)
+// 4 - revision (номер ревизии, берется из GIT)
+function getVersions(project)
+{
+    var filePath = project.sourceDirectory + "/VERSION";
+     if (!File.exists(filePath))
+         throw new Error(("File '{0}' not found").format(filePath));
+
+     var file = new TextFile(filePath, TextFile.ReadOnly);
+     var verStr = file.readLine().trim();
+
+     var regex = /^\d+\.\d+\.\d+$/
+     var r = verStr. match(regex);
+     if (r === null) {
+         var msg =  "Incorrect version format. Must be: 'major.minor.patch'. See file {0}";
+         throw new Error(msg.format(filePath));
+     }
+
+     var verParts = verStr.split('.').map(function(item){return parseInt(item, 10);});
+
+     var verRevision;
+     var process = new Process();
+     process.setWorkingDirectory(project.sourceDirectory);
+     if (process.exec("git", ["log", "-1", "--pretty=%h"], false) === 0)
+         verRevision = process.readLine().trim();
+
+    return [verStr, verParts[0], verParts[1], verParts[2], verRevision];
+}
