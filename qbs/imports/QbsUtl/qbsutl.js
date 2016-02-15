c384c740f70f5ffd8795faf3e98f6ac0a3183a74
Pavel Karelin <hkarel@yandex.ru>
2016-02-15 11:34:05 +0300

    qbsutl.js : Изменена функция getVersions()
    Изменения:
    1) В качестве входящего параметра используется filePath - путь до файла с версией;
    2) Информация о git-ревизии вынесена в отдельную функцию.

--- qbs/imports/QbsUtl/qbsutl.js
+++ qbs/imports/QbsUtl/qbsutl.js
@@ -22,36 +22,31 @@ String.prototype.format = function () {
     });
 };
 
-// Функция возвращает информацию о версии проекта в виде массива из пяти элементов.
-// Массив имеет следующие элементы:
+// Функция возвращает информацию о версии проекта в виде массива из четырех эле-
+// ментов. В качестве входящего параметра используется полное имя файла с версией.
+// Информация о версии должна быть записана в виде: 'major.minor.patch'.
+// Результирующий массив имеет следующие элементы:
 // 0 - версия проекта в формате major.minor.patch
 // 1 - major версия
 // 2 - minor версия
 // 3 - patch (версия исправлений)
-// 4 - revision (номер ревизии, берется из GIT)
-function getVersions(project)
+function getVersions(filePath)
 {
-    var filePath = project.sourceDirectory + "/VERSION";
-     if (!File.exists(filePath))
-         throw new Error(("File '{0}' not found").format(filePath));
+    //var filePath = project.sourceDirectory + "/VERSION";
+    if (!File.exists(filePath))
+        throw new Error(("File '{0}' not found").format(filePath));
 
-     var file = new TextFile(filePath, TextFile.ReadOnly);
-     var verStr = file.readLine().trim();
-
-     var regex = /^\d+\.\d+\.\d+$/
-     var r = verStr. match(regex);
-     if (r === null) {
-         var msg =  "Incorrect version format. Must be: 'major.minor.patch'. See file {0}";
-         throw new Error(msg.format(filePath));
-     }
-
-     var verParts = verStr.split('.').map(function(item){return parseInt(item, 10);});
-
-     var verRevision;
-     var process = new Process();
-     process.setWorkingDirectory(project.sourceDirectory);
-     if (process.exec("git", ["log", "-1", "--pretty=%h"], false) === 0)
-         verRevision = process.readLine().trim();
+    var file = new TextFile(filePath, TextFile.ReadOnly);
+    var verStr = file.readLine().trim();
+    var regex = /^\d+\.\d+\.\d+$/
+    var r = verStr. match(regex);
+    if (r === null) {
+        var msg =  "Incorrect version format. Must be: 'major.minor.patch'. See file {0}";
+        throw new Error(msg.format(filePath));
+    }
+    var verParts = verStr.split('.').map(function(item){return parseInt(item, 10);});
+    return [verStr, verParts[0], verParts[1], verParts[2]];
+}
 
     return [verStr, verParts[0], verParts[1], verParts[2], verRevision];
 }
