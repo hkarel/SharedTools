ea9981834164748d05ca9e884ed93449a931310a
Pavel Karelin <hkarel@yandex.ru>
2020-06-05 19:12:43 +0300

    list : Корректировка даты в MIT лицензии

--- list.h
+++ list.h
@@ -2,7 +2,7 @@
 /*****************************************************************************
   The MIT License
 
-  Copyright © 2005-2017 Pavel Karelin (hkarel), <hkarel@yandex.ru>
+  Copyright © 2005 Pavel Karelin (hkarel), <hkarel@yandex.ru>
 
   Permission is hereby granted, free of charge, to any person obtaining
   a copy of this software and associated documentation files (the
