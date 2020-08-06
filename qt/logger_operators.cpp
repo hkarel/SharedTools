febc82a03bb5de69dd23f5ecf4424c1044478fef
Pavel Karelin <hkarel@yandex.ru>
2020-08-06 15:18:16 +0300

    qt/logger_operators : Добавлен вывод в лог для типа QVector<QUuidEx>

--- qt/logger_operators.cpp
+++ qt/logger_operators.cpp
@@ -90,125 +90,147 @@ Line& operator<< (Line& line, const QHostAddress& h)
 }
 #endif
 
+template<typename List>
+void printList(Line& line, const List& list)
+{
+    line << "[";
+    for (int i = 0; i < list.count(); ++i)
+    {
+        if (i != 0)
+            line << ", ";
+        line << list[i];
+    }
+    line << "]";
+}
+
 Line& operator<< (Line& line, const QVariant& v)
 {
-    if (line.toLogger())
+    if (!line.toLogger())
+        return line;
+
+    if (v.type() == QVariant::Type(qMetaTypeId<float>()))
     {
-        if (v.type() == QVariant::Type(qMetaTypeId<float>()))
+        line << v.toFloat();
+        return line;
+    }
+
+    if (v.isNull())
+    {
+        line << "QVariant is null";
+        return line;
+    }
+
+    switch (v.type())
+    {
+        case QVariant::Invalid:
+            line << "Invalid QVariant";
+            break;
+
+        case QVariant::Bool:
+            line << v.toBool();
+            break;
+
+        case QVariant::ByteArray:
+            line << v.toByteArray();
+            break;
+
+        case QVariant::String:
         {
-            line << v.toFloat();
-            return line;
+            QString s = v.toString();
+            if (s.isEmpty())
+                s = "\"\"";
+            line << s;
+            break;
         }
+        case QVariant::Int:
+            line << v.toInt();
+            break;
 
-        if (v.isNull())
+        case QVariant::UInt:
+            line << v.toUInt();
+            break;
+
+        case QVariant::LongLong:
+            line << v.toLongLong();
+            break;
+
+        case QVariant::ULongLong:
+            line << v.toULongLong();
+            break;
+
+        case QVariant::Double:
+            line << v.toDouble();
+            break;
+
+        case QVariant::Time:
         {
-            line << "QVariant is null";
-            return line;
+            QTime t = v.toTime();
+            if (t.isValid())
+                line << t.toString(Qt::ISODate);
+            else
+                line << "Invalid QTime";
+            break;
         }
-
-        switch (v.type())
+        case QVariant::Date:
         {
-            case QVariant::Invalid:
-                line << "Invalid QVariant";
-                break;
-
-            case QVariant::Bool:
-                line << v.toBool();
-                break;
-
-            case QVariant::ByteArray:
-                line << v.toByteArray();
-                break;
-
-            case QVariant::String:
-            {
-                QString s = v.toString();
-                if (s.isEmpty())
-                    s = "\"\"";
-                line << s;
-                break;
-            }
-            case QVariant::Int:
-                line << v.toInt();
-                break;
-
-            case QVariant::UInt:
-                line << v.toUInt();
-                break;
-
-            case QVariant::LongLong:
-                line << v.toLongLong();
-                break;
-
-            case QVariant::ULongLong:
-                line << v.toULongLong();
-                break;
-
-            case QVariant::Double:
-                line << v.toDouble();
-                break;
-
-            case QVariant::Time:
-            {
-                QTime t = v.toTime();
-                if (t.isValid())
-                    line << t.toString(Qt::ISODate);
-                else
-                    line << "Invalid QTime";
-                break;
-            }
-            case QVariant::Date:
-            {
-                QDate d = v.toDate();
-                if (d.isValid())
-                    line << d.toString(Qt::ISODate);
-                else
-                    line << "Invalid QDate";
-                break;
-            }
-            case QVariant::DateTime:
-            {
-                QDateTime dt = v.toDateTime();
-                if (dt.isValid())
-                    line << dt.toString(Qt::ISODate);
-                else
-                    line << "Invalid QDateTime";
-                break;
-            }
+            QDate d = v.toDate();
+            if (d.isValid())
+                line << d.toString(Qt::ISODate);
+            else
+                line << "Invalid QDate";
+            break;
+        }
+        case QVariant::DateTime:
+        {
+            QDateTime dt = v.toDateTime();
+            if (dt.isValid())
+                line << dt.toString(Qt::ISODate);
+            else
+                line << "Invalid QDateTime";
+            break;
+        }
 #if QT_VERSION >= 0x050000
-            case QVariant::Uuid:
-            {
-                const QUuid& u = v.toUuid();
-                line << u;
-                break;
-            }
-#endif
-            case QVariant::UserType:
-            {
-                if (v.userType() == qMetaTypeId<QUuidEx>())
-                {
-                    const QUuidEx& u = v.value<QUuidEx>();
-                    line << u;
-                }
-
-#if QT_VERSION < 0x050000
-                else if (v.userType() == qMetaTypeId<QUuid>())
-                {
-                    const QUuid& u = v.value<QUuid>();
-                    line << u;
-                }
-#endif
-                else
-                {
-                    line << "Unsupported QVariant user-type for logger"
-                         << "; Type name: " << v.typeName();
-                }
-                break;
-            }
-            default:
-                line << "Unsupported QVariant type for logger"
-                     << "; Type name: " << v.typeName();
+        case QVariant::Uuid:
+        {
+            const QUuid& u = v.toUuid();
+            line << u;
+            break;
         }
+#endif
+        case QVariant::UserType:
+        {
+            if (v.userType() == qMetaTypeId<QUuidEx>())
+            {
+                const QUuidEx& u = v.value<QUuidEx>();
+                line << u;
+            }
+#if QT_VERSION < 0x050000
+            else if (v.userType() == qMetaTypeId<QUuid>())
+            {
+                const QUuid& u = v.value<QUuid>();
+                line << u;
+            }
+#endif
+            else if (v.userType() == qMetaTypeId<QVector<QUuid>>())
+            {
+                const QVector<QUuid>& vect = v.value<QVector<QUuid>>();
+                printList(line, vect);
+            }
+            else if (v.userType() == qMetaTypeId<QVector<QUuidEx>>())
+            {
+                const QVector<QUuidEx>& vect = v.value<QVector<QUuidEx>>();
+                printList(line, vect);
+            }
+            else
+            {
+                line << "Unsupported QVariant user-type for logger"
+                     << "; Type name: " << v.typeName();
+            }
+            break;
+        }
+        default:
+            line << "Unsupported QVariant type for logger"
+                 << "; Type name: " << v.typeName();
     }
     return line;
 }
