aac968a26d2f34898e4bfe5366645c5794f9c590
Pavel Karelin <hkarel@yandex.ru>
2020-09-02 20:27:03 +0300

    allocator_ptr : Рефакторинг кода

--- allocator_ptr.h
+++ allocator_ptr.h
@@ -25,28 +25,29 @@
   ---
 
   В модуле реализованы аллокаторы для инеллектуальных указатель simple_prt и
-  container_ptr.
+  container_ptr
 *****************************************************************************/
 
 #pragma once
 
 /**
-  @brief Типовой распределитель памяти для еденичного объекта.
-  Распределитель памяти не может иметь состояний и все его члены должны
+  Типовой распределитель памяти для еденичного объекта.
+  Распределитель памяти не может иметь состояний  и все его  члены  должны
   быть статическими. В противном случае возможны конфликты для конструкций
-  вида: container_ptr<base-class>(new derived-class).
+  вида: container_ptr<base-class>(new derived-class)
 */
 template<typename T> struct allocator_ptr
 {
     // Функция создания объектов.
     // Примечание: должно быть две функции create(). Если использовать только
-    // одну функцию вида T* create(const T* x = 0){return (x) ? new T(*x) : new T();}
-    // то компилятор будет требовать обязательное наличие конструктора копирования
-    // у инстанциируемого класса.
+    // одну функцию вида:
+    //   T* create(const T* x = 0) {return (x) ? new T(*x) : new T();}
+    // то  компилятор  будет  требовать  обязательное  наличие  конструктора
+    // копирования у инстанциируемого класса
     static T* create() {return new T();}
     static T* create(const T* x) {return (x) ? new T(*x) : new T();}
 
-    // Функция разрушения объектов.
+    // Функция разрушения объектов
     static void destroy(T* x) {
         static_assert(sizeof(T) > 0, "Can't delete pointer to incomplete type");
         delete x;
@@ -54,7 +55,7 @@ template<typename T> struct allocator_ptr
 
     // Функция используется при разрушении объектов хранимых в container_ptr,
     // join - признак использования единого сегмента памяти целевого объекта
-    // и экземпляра counter_ptr_t.
+    // и экземпляра counter_ptr_t
     static void destroy(T* x, bool join) {
         static_assert(sizeof(T) > 0, "Can't delete pointer to incomplete type");
         if (x) {if (join) x->~T(); else delete x;}
@@ -62,25 +63,28 @@ template<typename T> struct allocator_ptr
 };
 
 /**
-  @brief Типовой распределитель памяти для массива объектов.
-  Примечание: не использовать array-аллокаторы для container_ptr при условии,
-  что container_ptr будет создаваться с использованием функции create_join_ptr().
+  Типовой распределитель памяти для массива объектов
 */
 template<typename T> struct allocator_array_ptr
 {
     // Функция разрушения объектов
-    // Второй параметр введен для совместимости с аллокаторами container_ptr.
-    static void destroy(T* x /*, bool*/) {
+    static void destroy(T* x) {
         static_assert(sizeof(T) > 0, "Can't delete pointer to incomplete type");
         delete [] x;
     }
+
+    // Примечание: не используем array-аллокаторы для экземпляров container_ptr
+    // созданных при помощи  функции  create_join_ptr(), так как  в этом случае
+    // невозможно корректно освободить память
+    // static void destroy(T* x, bool join)
 };
 
 /**
-  @brief Используется для определения эквивалентных аллокаторов
+  Используется для определения эквивалентных аллокаторов.
   Примечание: реализация взята из STL cpp_type_traits.h
 */
-template<template<typename> class A1, template<typename> class A2 > struct allocator_ptr_equal
+template<template<typename> class A1,
+         template<typename> class A2 > struct allocator_ptr_equal
 {
     enum {Yes = 0};
 };
