8fcb16b04f96ae221cad588b4b632eb4434eca28
Pavel Karelin <hkarel@yandex.ru>
2020-10-12 16:58:22 +0300

    qt/qhashex : Для определения QHashEx, QSetEx используется директива 'using'

--- qt/qhashex.h
+++ qt/qhashex.h
@@ -33,11 +33,8 @@
   типами. Классы QHashEx, QSetEx исправляют эту ошибку.
 */
 #if QT_VERSION >= 0x050000
-// Утилита check_init некорректно обрабатывает директиву using
-// template<typename Key, typename T> using QHashEx = QHash<Key, T>;
-// template<typename T> using QSetEx = QSet<T>;
-template<typename Key, typename T> struct QHashEx : public QHash<Key, T> {};
-template<typename T> struct QSetEx : public QSet<T> {};
+template<typename Key, typename T> using QHashEx = QHash<Key, T>;
+template<typename T> using QSetEx = QSet<T>;
 #else
 template<typename T>
 struct QHashIntegral
