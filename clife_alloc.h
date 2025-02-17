5f247a19b81b83bf9083417a719a398398eba09b
Karelin Pavel <hkarel@yandex.ru>
2025-02-17 11:22:36 +0300

    clife_alloc : Добавлен аллокатор clife_alloc_ref

--- clife_alloc.h
+++ clife_alloc.h
@@ -44,3 +44,48 @@ template<typename T> struct clife_alloc
     }
     void destroy(clife_base* x) {if (x) x->release();}
 };
+
+/**
+  !!! Экспериментальный аллокатор !!!
+  clife_alloc_ref - вспомогательный класс, используется в контейнере lst::List
+  для создания и разрушения элементов списка унаследованных от clife_base.
+  В отличии от clife_alloc, аллокатор clife_alloc_ref позволяет минимизировать
+  код создания ссылочного списка.
+  Пример создания ссылочного списка при использовании clife_alloc:
+    lst::List<Element, Find, clife_alloc<Element>> List;
+    List list;
+
+    // Создание ссылочного списка ret_list:
+    List ret_list;
+    for (Element* e : list) {
+      e->add_ref();
+      ret_list.add(e);
+    }
+    return ret_list;
+
+  Пример создания ссылочного списка при использовании clife_alloc_ref:
+    lst::List<Element, Find, clife_alloc_ref<Element>> List;
+    List list;
+
+    // Создание неименованного ссылочного списка:
+    return list;
+
+  Примечание: метод T* create(T*) вызывается внутри функции lst::List::assign()
+*/
+template<typename T> struct clife_alloc_ref
+{
+    T* create() {
+        static_assert(std::is_base_of<clife_base, T>::value,
+                      "Type T must be derived from clife_base");
+        T* x = new T();
+        x->add_ref();
+        return x;
+    }
+    T* create(T* x) {
+        static_assert(std::is_base_of<clife_base, T>::value,
+                      "Type T must be derived from clife_base");
+        x->add_ref();
+        return x;
+    }
+    void destroy(clife_base* x) {if (x) x->release();}
+};
